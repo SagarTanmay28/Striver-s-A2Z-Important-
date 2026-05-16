d3e290692f620cf51a07cd50b014e9130e444ec7
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-05-16 15:31:17 +0530

    Create 05) Sieve of Eratothenes.cpp

---
+++ 02) Must Know/05) Sieve of Eratothenes.cpp
@@ -0,0 +1,16 @@
+TC: O(n log log n)
+SC: O(n)
+🔥 Best for 10⁵–10⁷ range
+
+vector<bool> sieve(int n) {
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * 1LL * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i)
+                isPrime[j] = false;
+        }
+    }
+    return isPrime;
+}
