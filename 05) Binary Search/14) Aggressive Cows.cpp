61ec84aa276ca5d0f86d82790be6554e9bc7d7b5
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-05-18 16:36:59 +0530

    Update 14) Aggressive Cows.cpp

--- 05) Binary Search/14) Aggressive Cows.cpp
+++ 05) Binary Search/14) Aggressive Cows.cpp
@@ -1,28 +1,27 @@
-Given an array arr[] where each element denotes the length of a board, and an integer k representing the number of painters available. Each painter takes 1 unit of time to paint 1 unit length of a board.
-
-Determine the minimum amount of time required to paint all the boards, under the constraint that each painter can paint only a contiguous sequence of boards (no skipping or splitting allowed).
+You are given an array with unique elements of stalls[], which denote the positions of stalls. You are also given an integer k 
+which denotes the number of aggressive cows. The task is to assign stalls to k cows such that the minimum distance between 
+any two of them is the maximum possible.
 
 Examples:
 
-Input: arr[] = [5, 10, 30, 20, 15], k = 3
-Output: 35
-Explanation: The optimal allocation of boards among 3 painters is - 
-Painter 1 → [5, 10] → time = 15
-Painter 2 → [30] → time = 30
-Painter 3 → [20, 15] → time = 35
-Job will be done when all painters finish i.e. at time = max(15, 30, 35) = 35
-Input: arr[] = [10, 20, 30, 40], k = 2
-Output: 60
-Explanation: A valid optimal partition is - 
-Painter 1 → [10, 20, 30] → time = 60
-Painter 2 → [40] → time = 40
-Job will be complete at time = max(60, 40) = 60
-Input: arr[] = [100, 200, 300, 400], k = 1
-Output: 1000
-Explanation: There is only one painter, so the painter must paint all boards sequentially. The total time taken will be the sum of all board lengths, i.e., 100 + 200 + 300 + 400 = 1000.
+Input: stalls[] = [1, 2, 4, 8, 9], k = 3
+Output: 3
+Explanation: The first cow can be placed at stalls[0], 
+the second cow can be placed at stalls[2] and 
+the third cow can be placed at stalls[3]. 
+The minimum distance between cows in this case is 3, which is the largest among all possible ways.
+Input: stalls[] = [10, 1, 2, 7, 5], k = 3
+Output: 4
+Explanation: The first cow can be placed at stalls[0],
+the second cow can be placed at stalls[1] and
+the third cow can be placed at stalls[4].
+The minimum distance between cows in this case is 4, which is the largest among all possible ways.
+Input: stalls[] = [2, 12, 11, 3, 26, 7], k = 5
+Output: 1
+Explanation: There are 6 stalls and only 5 cows, we try to place the cows such that the minimum distance between any two cows is as large as possible.
+The minimum distance between cows in this case is 1, which is the largest among all possible ways.
 
-
-
+  
 class Solution {
   public:
     int check(int mid, int k, vector<int>& nums){
