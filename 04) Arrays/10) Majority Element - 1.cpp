0ac0082a15a49168717304c48697f21f1f7082ae
SAGAR TANMAY SATYAVRATA <247439393+SagarTanmay28@users.noreply.github.com>
2026-08-04 11:20:44 +0530

    Update 10) Majority Element - 1.cpp

--- 04) Arrays/10) Majority Element - 1.cpp
+++ 04) Arrays/10) Majority Element - 1.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int n = nums.size();
 
-        int count = 0;
-        int ele = 0;
+        int ele   = nums[0];
+        int count = 1;
 
-        for(int i = 0; i < nums.size(); i++) {
+        for(int i=1 ;i<n ;i++){
 
-            if(count == 0) {
+            if(count == 0){
                 ele = nums[i];
+                count = 1;
             }
-
-            if(nums[i] == ele) {
-                count++;
-            }
-            else {
-                count--;
-            }
+            else if(ele == nums[i]) count++;
+            else count--;
         }
 
         return ele;
